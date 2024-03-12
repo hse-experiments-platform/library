5328fd934fdc7d6625060227f18a649ab8d88df7
ltulyavko <wot.k@mail.ru>
2024-03-12 23:29:07 +0300

    add middleware

--- pkg/utils/token/interface.go
+++ pkg/utils/token/interface.go
@@ -1,6 +1,10 @@
 package token
 
-import "time"
+import (
+	"time"
+
+	"google.golang.org/grpc"
+)
 
 type Maker interface {
 	CreateToken(userID int64, roles []string, duration time.Duration) (string, error)
