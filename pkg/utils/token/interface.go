6db27bc1327b566d95868e9b384b2d6f9456d923
ltulyavko <wot.k@mail.ru>
2024-03-12 23:28:30 +0300

    add middleware

--- pkg/utils/token/interface.go
+++ pkg/utils/token/interface.go
@@ -5,4 +5,5 @@ import "time"
 type Maker interface {
 	CreateToken(userID int64, roles []string, duration time.Duration) (string, error)
 	VerifyToken(token string) (*Payload, error)
+	TokenExtractorUnaryInterceptor() grpc.UnaryServerInterceptor
 }
